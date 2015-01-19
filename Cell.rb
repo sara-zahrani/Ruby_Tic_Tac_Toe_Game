180ed0a596ae9a6b354715f9f937d298c9610b4a
Sara Z <sara.z@tamprojects.com>
2015-01-19 20:12:25 +0300

    Add Cell.rb class structure

---
+++ Cell.rb
@@ -0,0 +1,15 @@
+class Cell
+
+  attr_reader :cell_id
+
+  def initialize()
+    @cell_id = ''
+  end
+
+  # Check if cell is empty 
+  # @return [Bool] true if empty, false otherwise.
+  def empty?
+    
+  end
+
+end # class Cell end 
