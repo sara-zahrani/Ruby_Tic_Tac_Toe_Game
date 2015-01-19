b178e60bd3ff7968991734bfd41de4773dc9eb67
Sara Z <sara.z@tamprojects.com>
2015-01-20 01:18:29 +0300

    Add draw_board method

--- Board.rb
+++ Board.rb
@@ -6,12 +6,21 @@ class Board
 	attr_reader :board
 
 	def initialize
-		@board = [Cell.new().cell_id] * 8
+		@board = [Cell.new().cell_id] * 9
 	end	
 
-  # Draw the board on the terminal
+  # Draw the board on the terminal #{@board[0]}
+  # @todo Think of a better way to draw the baord
   def draw_board
-		# @todo draw the board 
+    puts "\t#{@board[0]}\t|\t#{@board[1]}\t|\t#{@board[2]}"
+    puts "\t\t|\t\t|\t"
+    puts "\t-------------------------------------"
+    puts "\t#{@board[3]}\t|\t#{@board[4]}\t|\t#{@board[5]}"
+    puts "\t\t|\t\t|\t"
+    puts "\t-------------------------------------"   
+    puts "\t#{@board[6]}\t|\t#{@board[7]}\t|\t#{@board[8]}"
+    puts "\t\t|\t\t|\t"
+    puts "\t-------------------------------------"
 	end
 
   # Check if all cells in the board are all empty.
@@ -27,3 +36,6 @@ class Board
 	end
 
 end # class Board end 
+
+board = Board.new
+board.draw_board
